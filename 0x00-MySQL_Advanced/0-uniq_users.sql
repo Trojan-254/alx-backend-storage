350585df8fa4ee07d24008a1079ca97a30eaee2d
Samwuel simiyu <samwuels254@gmail.com>
2024-10-17 20:43:37 +0000

    0. We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,7 @@
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
 
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    email VARCHAR(255) UNIQUE,
    name  VARCHAR(255)
